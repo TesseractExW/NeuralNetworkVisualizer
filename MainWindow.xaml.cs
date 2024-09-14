1d954ca59bb2c6e7f8c8c1fed945df452d6c7f4e
jjheadshotsniper <w.pawinee17@gmail.com>
2024-09-14 18:22:13 +0700

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using NVV2.UserControls;
+using System.Diagnostics;
+using System.Windows;
+
+namespace NVV2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Random rnd = new Random();
+            float[] input_values = new float[16 * 16];
+            inputNode.LeftClicked.Add((int x, int y) =>
+            {
+                int r_x = (int)(x / inputNode.ActualWidth * inputNode.dimensionX);
+                int r_y = (int)(y / inputNode.ActualHeight * inputNode.dimensionY);
+                if (r_x < inputNode.dimensionX && r_y < inputNode.dimensionY)
+                {
+                    inputNode.DrawPixel(r_x, r_y, 255, 255, 255);
+                    input_values[r_y * inputNode.dimensionX + r_x] = 1.0f;
+                    Debug.WriteLine("Set pixel at (" + r_x.ToString() + " , " + r_y.ToString() + ") : " + input_values[r_y * inputNode.dimensionX + r_x].ToString());
+                }
+            });
+            inputNode.RightClicked.Add((int x, int y) =>
+            {
+                int r_x = (int)(x / inputNode.ActualWidth * inputNode.dimensionX);
+                int r_y = (int)(y / inputNode.ActualHeight * inputNode.dimensionY);
+                if (r_x < inputNode.dimensionX && r_y < inputNode.dimensionY)
+                {
+                    inputNode.DrawPixel(r_x, r_y, 0, 0, 0);
+                    input_values[r_y * inputNode.dimensionX + r_x] = 0.0f;
+                    Debug.WriteLine("Delete pixel at (" + r_x.ToString() + " , " + r_y.ToString() + ") : " + input_values[r_y * inputNode.dimensionX + r_x].ToString());
+                }
+            });
+            Visualizer[] arr = { node_0, node_1, node_2, node_3 };
+            foreach (Visualizer control in arr)
+            {
+                for (int i = 0; i < inputNode.dimensionX; i++)
+                {
+                    for (int j = 0; j < inputNode.dimensionY; j++)
+                    {
+                        int val = (int)(255 * rnd.NextDouble());
+                        control.DrawPixel(i, j, val, val, val);
+                    }
+                }
+            }
+            /*CompositionTarget.Rendering += (e, o) =>
+            {
+
+            };*/
+        }
+    }
+}
---
+++ UserControls/Visualizer.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NVV2.UserControls
+{
+    /// <summary>
+    /// Interaction logic for Visualizer.xaml
+    /// </summary>
+    public partial class Visualizer : UserControl
+    {
+        public delegate void ClickHandler(int x, int y);
+        public List<ClickHandler> LeftClicked = new List<ClickHandler>();
+        public List<ClickHandler> RightClicked = new List<ClickHandler>();
+        public int dimensionX = 16;
+        public int dimensionY = 16;
+        private void MouseMove(object sender, MouseEventArgs e)
+        {
+            Point p = e.GetPosition(picture);
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                foreach (ClickHandler clickHandler in LeftClicked) clickHandler((int)Math.Ceiling(p.X), (int)Math.Ceiling(p.Y));
+            }
+            else if (e.RightButton == MouseButtonState.Pressed)
+            {
+                foreach (ClickHandler clickHandler in RightClicked) clickHandler((int)Math.Ceiling(p.X), (int)Math.Ceiling(p.Y));
+            }
+        }
+        public Visualizer()
+        {
+            InitializeComponent();
+            RenderOptions.SetBitmapScalingMode(picture, BitmapScalingMode.NearestNeighbor);
+            RenderOptions.SetEdgeMode(picture, EdgeMode.Unspecified);
+            bitmap = new WriteableBitmap(dimensionX, dimensionY, 96, 96, PixelFormats.Bgr32, null);
+            picture.Source = bitmap;
+            picture.MouseMove += new MouseEventHandler(MouseMove);
+            picture.MouseLeftButtonDown += new MouseButtonEventHandler(
+                (sender, e) => {
+                    Point p = e.GetPosition(picture);
+                    foreach (ClickHandler clickHandler in LeftClicked) clickHandler((int)Math.Ceiling(p.X), (int)Math.Ceiling(p.Y));
+                }
+            );
+            picture.MouseRightButtonDown += new MouseButtonEventHandler(
+                (sender, e) => {
+                    Point p = e.GetPosition(picture);
+                    foreach (ClickHandler clickHandler in RightClicked) clickHandler((int)Math.Ceiling(p.X), (int)Math.Ceiling(p.Y));
+                }
+            );
+        }
+        public WriteableBitmap bitmap;
+        public void DrawPixel(int x,int y,int r,int g,int b)
+        {
+            try
+            {
+                bitmap.Lock();
+                unsafe
+                {
+                    IntPtr pBackBuffer = bitmap.BackBuffer;
+                    pBackBuffer += y * bitmap.BackBufferStride;
+                    pBackBuffer += x * 4;
+
+                    int color_data = r << 16;
+                    color_data |= g << 8;
+                    color_data |= b << 0;
+                    *((int*)pBackBuffer) = color_data;
+                }
+                bitmap.AddDirtyRect(new Int32Rect(x, y, 1, 1));
+            }
+            finally
+            {
+                bitmap.Unlock();
+            }
+        }
+        public void Update()
+        {
+            picture.UpdateLayout();
+        }
+    }
+}
